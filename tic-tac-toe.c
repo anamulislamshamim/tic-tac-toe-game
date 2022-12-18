2a67fcb476a9e9e8ac674bee4ac00c5f6d0e3ce7
unknown <anamulislamshamim117@gmail.com>
2022-12-19 05:30:24 +0600

    user win checker function implemented successfully

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -15,6 +15,40 @@ void ui(int matrix[][3], int n, int m)
     };
 };
 
+// check who is win
+/*
+
+0   x   0
+x   0   x
+0   x   0
+
+*/
+int whoIsWin(int matrix[][3], int n)
+{
+
+    int i,j;
+    for(i=0;i<n;i++){
+        if(matrix[i][0] == matrix[i][1] && matrix[i][1]  == matrix[i][2] && matrix[i][0] != -1){
+            return matrix[i][0];
+        };
+    };
+    for(j=0;j<n;j++){
+        if(matrix[0][j]==matrix[1][j] && matrix[1][j] == matrix[2][j] && matrix[0][j] != -1){
+            return matrix[0][j];
+        };
+    };
+    if(matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2] && matrix[0][0]!=-1){
+        return matrix[0][0];
+    };
+    if(matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0] && matrix[1][1] != -1){
+        return matrix[1][1];
+    };
+
+    return -1;
+
+};
+
+
 int main()
 {
     int n=3,m=3;
@@ -27,6 +61,7 @@ int main()
     };
     bool player1=true;
     bool player2=false;
+    // take user input:
     while(true){
         ui(matrix, n, m);
         if(player1){
@@ -38,7 +73,7 @@ int main()
                 printf("Invalid Cell\n");
                 goto Flag1;
             }
-            matrix[r][c]=2;
+            matrix[r][c]=2; 
             player1=false;
             player2=true;
         }else{
@@ -53,6 +88,14 @@ int main()
             matrix[r][c]=1;
             player1=true;
             player2=false;
+        };
+        // check who is win
+        if(whoIsWin(matrix, n) == 1){
+            printf("\n\nPlayer 1 is won!\n\n");
+            break;
+        }else if(whoIsWin(matrix, n) == 2){
+            printf("\n\nPlayer 2 is won!\n\n");
+            break;
         }
     };   
     ui(matrix, n, m);
