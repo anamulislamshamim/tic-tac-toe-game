d69b2fbc3b502aee0e5999dd6b545266c5fe4ebd
unknown <anamulislamshamim117@gmail.com>
2022-12-18 11:44:56 +0600

    created the UI for the game

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -2,6 +2,25 @@
 
 int main()
 {
-    printf("start the tic-toc-toe game");
+    int n=3,m=3;
+    int matrix[n][m];
+    int i,j;
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            matrix[i][j]= -1;
+        };
+    };
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            if(matrix[i][j] != -1) {
+                printf("%d",matrix[i][j]);
+            }else{
+                printf(" ");
+            };
+            if(j<n-1)printf("\t|\t");
+        };
+        if(i<n-1)printf("\n__________________________________");
+        printf("\n\n");
+    };
     return 0;
 }
